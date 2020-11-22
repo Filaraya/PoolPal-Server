8884e84bb8454c61f253086eeeffa2b6b5d16773
Kyle Anderson <kylea@schwebdesign.com>
2020-11-22 10:01:19 -0500

    adding basic calculation

--- routers/chemicals.js
+++ routers/chemicals.js
@@ -2,6 +2,9 @@ var express = require('express');
 var User = require('../models/user');
 var chemicalRouter = express.Router();
 
+const targetPh = 7.4;
+const phUpAmountOz = 60;
+
 chemicalRouter
     .route('/chemicals')
     .get(function (request, response) {
@@ -15,7 +18,17 @@ chemicalRouter
         var ph = request.body.ph;
         var userId = request.body.user_id;
 
-        var user = User.findOne({ _id: userId });
+        User.findOne({ _id: userId }, function (error, user) {
+            if (error) {
+                response.status(500).send(error);
+                return;
+            }
+
+            console.log('Add ' + (targetPh - ph) * phUpAmountOz + ' oz of ' + user.chemicals[0].ph_up);
+            console.log(user);
+            console.log(alkalinity);
+        });
+
 
         console.log(user);
 
